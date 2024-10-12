3e9b3c66de55a6b18bc3f2b03f303bde2dfe66ab
Freddy Ayón <32682860+freddoOswaldo@users.noreply.github.com>
2024-10-12 12:44:08 -0500

    se guarda el usuario en base

--- lib/login.dart
+++ lib/login.dart
@@ -1,53 +1,43 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_apps/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  initFirebase() async {
-    return await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  }
-
   Future<UserCredential> signInWithGoogle() async {
     try {
-      // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-      // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
           await googleUser?.authentication;
-
-      // Create a new credential
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
-
-      // Once signed in, return the UserCredential
-      return await FirebaseAuth.instance.signInWithCredential(credential);
+      final dataUser = await FirebaseAuth.instance.signInWithCredential(credential);
+      FirebaseFirestore.instance
+      .collection('users')
+      .add({
+        "email": dataUser.user?.email,
+        "name": dataUser.user?.displayName,
+        "photo": dataUser.user?.photoURL,
+        "phone": dataUser.user?.phoneNumber,
+      });
+      return dataUser;
     } on FirebaseAuthException catch (e) {
-      print(e.message);
       throw e;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    initFirebase();
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           ElevatedButton(
-            onPressed: () async {
-                final UserCredential f = await signInWithGoogle();
-                print(f);
-            }, // el null deshabilita el botón
+            onPressed: signInWithGoogle, // el null deshabilita el botón
             child: const Text('Iniciar sesion con Google'),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/firebase_options.dart';
 import 'package:flutter_apps/login.dart';
 import 'package:flutter_apps/screens/area.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_apps/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,15 +12,25 @@ void main() async {
   runApp(const MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
+
+    initFirebase() async {
+    return await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+    initFirebase();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/home": (context) => const AreaScreen(),
         "/login": (context) => const LoginScreen(),
