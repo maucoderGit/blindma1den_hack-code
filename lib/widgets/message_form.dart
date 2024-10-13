05191816e2347802cf3ef6ae0ff4833a646a2e36
Freddy Ayón <32682860+freddoOswaldo@users.noreply.github.com>
2024-10-13 10:53:35 -0500

    cambio de estilo del popup

--- lib/widgets/message_form.dart
+++ lib/widgets/message_form.dart
@@ -6,19 +6,18 @@ import 'package:flutter_apps/models/review.dart';
 import 'package:flutter_apps/models/user.dart';
 import 'package:flutter_apps/screens/areac.dart';
 import 'package:flutter_apps/widgets/custom_text_input.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Widget registerMessageWidget(
-    double minExtent,
-    double maxExtent,
-    double initialExtent,
-    UserSelection selection,
-    double latitude,
-    double longitude,
-    DraggableScrollableController sheetController,
-    BuildContext context,
-    Function reloadPoint,
-    UserDB user,
+  double minExtent,
+  double maxExtent,
+  double initialExtent,
+  UserSelection selection,
+  double latitude,
+  double longitude,
+  DraggableScrollableController sheetController,
+  BuildContext context,
+  Function reloadPoint,
+  UserDB user,
 ) {
   String message = "";
 
@@ -27,86 +26,110 @@ Widget registerMessageWidget(
       minChildSize: minExtent,
       maxChildSize: maxExtent,
       initialChildSize: initialExtent,
-      builder:
-      (BuildContext context, ScrollController scrollController) => Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: Theme.of(context).canvasColor,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-        ),
-      ),
-      child: SingleChildScrollView(
-        controller: scrollController, child: Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Comparte tu experiencia!"),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(width: MediaQuery.of(context).size.width / 1.15, child: Card(
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(
-                      color: Color.fromRGBO(191, 191, 191, 1.0)
-                  ),
-                  borderRadius: cardRadius(),
+      builder: (BuildContext context, ScrollController scrollController) =>
+          Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
                 ),
-                child: CustomTextInput(
-                  hintText: 'Describe tu experiencia con todos los usuarios. \n\nTu mensaje puede ahorrar muchos malos momentos.', userTyped: (value) {
-                    message = value;
-                  }, obscure: false, defaultValue: "",
-                ),
-              )),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    disabledBackgroundColor: Colors.transparent,
-                    disabledForegroundColor: Colors.grey
-                ),
-                onPressed: () async {
-                  try {
-                    Review coordinateReview = Review(
-                      email: user.email,
-                      message: "",
-                      longitude: longitude,
-                      latitude: latitude,
-                      writeDate: DateTime.now(),
-                      storedMessages: [{
-                        "message": message,
-                        "email": user.email,
-                        "write_date": DateTime.now(),
-                      }]
-                    );
-
-                    FirebaseFirestore.instance.collection('zoneReviews').add(coordinateReview.toFirestore());
-
-                    sheetController.animateTo(0.0,
-                        duration: const Duration(milliseconds: 200), curve: Curves.bounceIn);
-
-                        reloadPoint();
-                  } on FirebaseAuthException catch (e) {
-                    rethrow;
-                  }
-                },
-                child: const Text("Enviar mensaje"),
               ),
-            ],
-          ),
-        ],
-      )),
-  )));
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ListTile(
+                          leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image(
+                          image: NetworkImage(
+                            user.photo!,
+                          ),
+                        )),
+                          title: const Text("Comparte tu experiencia!"),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width / 1.15,
+                                child: Card(
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                    side: const BorderSide(
+                                        color:
+                                            Color.fromRGBO(191, 191, 191, 1.0)),
+                                    borderRadius: cardRadius(),
+                                  ),
+                                  child: CustomTextInput(
+                                    hintText:
+                                        'Describe tu experiencia con todos los usuarios. \n\nTu mensaje puede ahorrar muchos malos momentos.',
+                                    userTyped: (value) {
+                                      message = value;
+                                    },
+                                    obscure: false,
+                                    defaultValue: "",
+                                  ),
+                                )),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  foregroundColor: Colors.black,
+                                  disabledBackgroundColor: Colors.transparent,
+                                  disabledForegroundColor: Colors.grey),
+                              onPressed: () async {
+                                try {
+                                  Review coordinateReview = Review(
+                                      email: user.email,
+                                      message: "",
+                                      longitude: longitude,
+                                      latitude: latitude,
+                                      writeDate: DateTime.now(),
+                                      storedMessages: [
+                                        {
+                                          "message": message,
+                                          "email": user.email,
+                                          "write_date": DateTime.now(),
+                                        }
+                                      ]);
+
+                                  FirebaseFirestore.instance
+                                      .collection('zoneReviews')
+                                      .add(coordinateReview.toFirestore());
+
+                                  sheetController.animateTo(0.0,
+                                      duration:
+                                          const Duration(milliseconds: 200),
+                                      curve: Curves.bounceIn);
+
+                                  reloadPoint();
+                                } on FirebaseAuthException catch (e) {
+                                  rethrow;
+                                }
+                              },
+                              child: const Text("Enviar mensaje"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )),
+              )));
 }
